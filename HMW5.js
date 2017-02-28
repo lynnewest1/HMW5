df3163538bcf1275a570f5d4bee81353ff67cfac
lynnethomas <westlynne@hotmail.comgit config --global user.email westlynne@hotmail.com>
2017-02-28 14:32:02 -0800

    added money stuff

--- HMW5.js
+++ HMW5.js
@@ -7,6 +7,21 @@ var newTotal = +origTotal + 2;
 document.getElementById("Total").innerHTML = newTotal;
 
 }
-function clearOrder() {
 
+function clearOrder() {
+    document.getElementById("Total").innerHTML = 0;
+
+}
+
+function addInputs() {
+    var currTotal = document.getElementById("Total").innerHTML;
+    if (isNaN(currTotal)) {
+        currTotal - 0;
+
+var myInput = document.getElementById("myinput").value;
+
+var newTotal = +myInput + +currTotal;
+
+document.getElementById("Total").innerHTML = newTotal;
+    }
 }
